ed52a2e1bbd5d246572604d115041307a78ed152
Philip R Butler <philip.r.butler@gmail.com>
2020-02-19 15:34:58 +0000

    add links to test blog posts on home page

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,51 +1,86 @@
-import React from "react"
-import { Link, StaticQuery, grapgql } from "gatsby"
+import React from 'react';
+import { Link, graphql } from 'gatsby';
+import Layout from '../components/layout';
+import Image from '../components/image';
+import SEO from '../components/seo';
 
-import Layout from "../components/layout"
-import Image from "../components/image"
-import SEO from "../components/seo"
+// const TitleAndDescription = ({ data }) => {
+//   const { title, description } = data.site.siteMetadata;
+//
+//   return (
+//     <div>
+//       <h2>{title}</h2>
+//       <p>{description}</p>
+//     </div>
+//   );
+// };
 
-const TitleAndDescription = ({ data }) => {
-	const {title, description} = data.site.siteMetadata;
+// const Header = () => {
+//   return (
+//     <StaticQuery
+//       query={graphql`
+//         query {
+//           site {
+//             siteMetadata {
+//               title
+//               description
+//             }
+//           }
+//         }
+//       `}
+//       render={data => <TitleAndDescription data={data} />}
+//     />
+//   );
+// };
 
-	return (
-		<div>
-			<h2>{title}</h2>
-			<p>{description}</p>
-		</div>
-	);
-}
+const IndexPage = ({ data }) => {
+  const { edges } = data.allMarkdownRemark;
+  return (
+    <Layout>
+      <SEO title="Home" />
+      <h1>Hello world</h1>
+      {edges.map(edge => {
+        const {
+          node: {
+            frontmatter: { title, path },
+          },
+        } = edge;
+        return (
+          <div key={path}>
+            <Link to={path}>{title}</Link>
+          </div>
+        );
+      })}
+      <p>Welcome to your new Gatsby site.</p>
+      <p>Now go build something great.</p>
+      <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
+        <Image />
+      </div>
+      <Link to="/page-2/">Go to page 2</Link>
+    </Layout>
+  );
+};
 
-const Header = () => {
-	return (
-		<StaticQuery 
-			query={
-				graphql`query{
-			  	site {
-				    siteMetadata {
-				      title
-				      description
-				    }
-			  	}
-				}`
-			}
-			render={(data) => <TitleAndDescription data={data} />}
-		/>
-	)	
-}
+export const query = graphql`
+  query HomePageQuery {
+    allMarkdownRemark(
+      sort: {
+        fields: [frontmatter___date, frontmatter___title]
+        order: [DESC, ASC]
+      }
+    ) {
+      edges {
+        node {
+          id
+          frontmatter {
+            title
+            path
+            date
+          }
+        }
+      }
+    }
+  }
+`;
 
-const IndexPage = () => (
-  <Layout>
-    <SEO title="Home" />
-    <h1>Hello world</h1>
-    <Header />
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
-    </div>
-    <Link to="/page-2/">Go to page 2</Link>
-  </Layout>
-)
-
-export default IndexPage
+export default IndexPage;
